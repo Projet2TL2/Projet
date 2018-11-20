9e4096334c5ef9c5ed9dfc0521640c376a604aa8
SebastienConotte <44473643+SebasteinConotte@users.noreply.github.com>
2018-11-20 15:29:03 +0100

    Test modifiction classe via github

--- Attaque.java
+++ Attaque.java
@@ -19,4 +19,50 @@ public class Attaque {
 		this.cout = cout;
 		this.centre = centre;
 	}
+	
+	/*
+	 * Crée une Attaque 
+	 * @param : nom represente le nom de l'attaque
+	 * @param : cout represente son cout 
+	 * @param : centre represente le centre de l'attaque
+	 */
+	public Attaque(String nom, int cout, Case centre) {
+		this.nom = nom;
+		this.cout = cout;
+		this.centre = centre;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Case[] getSurface() {
+		return surface;
+	}
+
+	public void setSurface(Case[] surface) {
+		this.surface = surface;
+	}
+
+	public int getCout() {
+		return cout;
+	}
+
+	public void setCout(int cout) {
+		this.cout = cout;
+	}
+
+	public Case getCentre() {
+		return centre;
+	}
+
+	public void setCentre(Case centre) {
+		this.centre = centre;
+	}
+	
+	
 }
