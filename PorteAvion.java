7bef96fe92d4787627e34a3ca31ab79d9b69e78c
GillesTimothy <44493111+GillesTimothy@users.noreply.github.com>
2018-11-20 10:22:58 +0100

    Add files via upload
    ajout classe PorteAvion

---
+++ PorteAvion.java
@@ -0,0 +1,29 @@
+package batailleNavale;
+
+public class PorteAvion extends Bateau{
+	private String nom;
+	private Case[] surface;
+	private Case centre;
+	
+	/*
+	 * crée un bateau
+	 * @param : nom représente "le nom du bateau"
+	 * @param : surface représente la surface du bateau
+	 * @param : centre représente le centre du bateau
+	 */
+	public PorteAvion(Case centre) {
+		super(centre);
+		this.nom = "porte-avion";
+		calculSurface(centre);
+		
+						
+	}
+    /**
+     * donne la surface du bateau
+     * @param centre
+     */
+	private void calculSurface(Case centre) {
+		this.surface  = new Case[5];
+		surface[3] = centre;
+	}
+}
