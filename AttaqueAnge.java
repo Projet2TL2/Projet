348ea70bfef1b4c6de119b4bad64866b17ab04e0
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2018-11-22 16:04:22 +0100

    Update AttaqueAnge.java

--- AttaqueAnge.java
+++ AttaqueAnge.java
@@ -1,7 +1,7 @@
 public class AttaqueAnge extends Attaque{
 
 	/*
-	 * Crée une Attaque Ultime des anges
+	 * CrÃ©e une Attaque Ultime des anges
 	 * @param : nom represente "Croix de Jesus"
 	 * @param : surface represente la surface de l'attaque (6 cases)
 	 * @param : cout represente son cout (10)
@@ -12,12 +12,12 @@ public class AttaqueAnge extends Attaque{
 	}
 
 	/*
-	 * Crée une Attaque Ultime des anges
+	 * CrÃ©e une Attaque Ultime des anges
 	 * @param : nom represente "Croix de Jesus"
 	 * @param : cout represente son cout (10)
 	 * @param : centre le centre de l'attaque
 	 */
-	public AttaqueHorizontale(String nom, int cout, Case centre) {
+	public AttaqueAnge(String nom, int cout, Case centre) {
 		super(nom,cout,centre);
 		calculSurface(centre);
 	}
@@ -27,8 +27,8 @@ public class AttaqueAnge extends Attaque{
 		surface[0] = new Case(centre.getColonne()-1,centre.getLigne());
 		surface[1] = centre;
 		surface[2] = new Case(centre.getColonne()+1,centre.getLigne());
-		surface[3] = new Case(centre.getLigne()-1,centre.getColonne());
-		surface[4] = new Case(centre.getLigne()+1,centre.getColonne());
-		surface[5] = new Case(centre.getLigne()+2,centre.getColonne());
+		surface[3] = new Case(centre.getColonne(),centre.getLigne()-1);
+		surface[4] = new Case(centre.getColonne(),centre.getLigne()+1);
+		surface[5] = new Case(centre.getColonne(),centre.getLigne()+2);
 	}
 }
