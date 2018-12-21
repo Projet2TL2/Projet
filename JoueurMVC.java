50128abdc335707323f61e7e81dd3bfb2dbc792a
SebastienConotte <44473643+SebasteinConotte@users.noreply.github.com>
2018-12-21 09:52:21 +0100

    Ajout dans le bon package

--- JoueurMVC.java
+++ JoueurMVC.java
@@ -1,8 +1,9 @@
-package model;
+
 
 import javax.swing.JOptionPane;
 
-import Controller.JoueurControl;
+import Controleur.JoueurControl;
+import Model.Joueur;
 import Vue.JoueurVue;
 import Vue.JoueurVueConsole;
 import Vue.JoueurVueGUIVersionReseau;
@@ -17,7 +18,6 @@ public class JoueurMVC {
 		JoueurControl ctrlGUI = new JoueurControl(model);
 		JoueurControl ctrlConsole = new JoueurControl(model);
 		
-		//Network test = new Network(model, arg);
 		JoueurVue gui = new JoueurVueGUIVersionReseau(model, ctrlGUI, isServeur);
 		JoueurVue console = new JoueurVueConsole(model, ctrlConsole);
 		
@@ -26,13 +26,7 @@ public class JoueurMVC {
 	}
 	
 	public static void main(String args[]) {
-		int nbr = JOptionPane.showConfirmDialog(null, "Attendez-vous qqun?", "Etes vous serveur?", JOptionPane.YES_NO_OPTION);
-		if(nbr == 0) {
-			isServeur = true;
-		}
-		else {
 			isServeur = false;
-		}
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new JoueurMVC(isServeur);
